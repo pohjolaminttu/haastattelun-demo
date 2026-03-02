bf4efc597175554b18eaec1b30aaecc2c16dcb1b
Minttu Pohjola <minttu.pohjola@gmail.com>
2026-03-02 15:18:47 +0200

    Menu näkymässä napista siirrytään info-sivulle, joka atm tulostaa valitun maan tiedot vasta konsoliin

--- src/pages/info.jsx
+++ src/pages/info.jsx
@@ -1,22 +1,33 @@
 //Sivu, jolla näytetään tilamuuttujassa olevan maan infot (jotka haetaan palvelimelta)
 import { useState, useEffect } from "react";
+import { useNavigation, useLocation } from 'react-router';
 import axios from "axios";
 
-const Info = ({country}) => {
+const Info = () => {
     const [infos, setInfos] = useState([]);
+    const selectedCountry = useLocation().state?.country;
+
     useEffect(() => {
         console.log('Effect for axios')
-        axios
-            .get(`https://restcountries.com/v3.1/all?fields=name/${country}`)
-            .then(response => {
-                console.log("Promise fulfilled :)")
-                setInfos(response.data)
-            })
-    }, []);
+        if (selectedCountry?.common) {
+            {/**Hakee tiedot vain siltä maalta, jonka "name" olio vastaa Info:lle välitettyä oliota */}
+            axios
+                .get(`https://restcountries.com/v3.1/name/${selectedCountry.common}?fullText=true`)
+                .then(response => {
+                    console.log("Promise fulfilled :)")
+                    console.log("Palvelimen vastaus (olio):", response.data);
+                    setInfos(response.data);
+                })
+                .catch(err => console.log("Virhe haussa:", err));
+            }
+        }, [selectedCountry]);
 
-    return (
-        <p>{infos}</p>
-    );
+
+return (
+    <div>
+        <p>Tiedot (ei vielä nähtävillä)</p>
+    </div>
+);
 };
 
 export default Info
--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -1,13 +1,15 @@
 //Lähtötilanne, jossa valitaan maa (sekä ylläpidetään tilamuuttujana valittua maata) ja siirrytään sivulle /info
 
 import { useState, useEffect, use } from "react";
+import { useNavigate } from 'react-router';
 import axios from "axios";
 import Select from 'react-select';
-import Info from "./info";
+import Button from 'react-bootstrap/Button';
 
 const Menu = () => {
     const [selectedCountry, setSelectedCountry] = useState(null);
     const [countryNames, setCountryNames] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         console.log('Effect for axios')
@@ -21,7 +23,7 @@ const Menu = () => {
 
     return (
         <div>
-
+            {/** Alasvetovalikko, joka näyttää maiden common nimet listana. Hakutoiminto, jolla voi hakea maan nimeä */}
             <Select
                 className="basic-single"
                 classNamePrefix="select"
@@ -35,14 +37,16 @@ const Menu = () => {
                 onChange={(selected)=>setSelectedCountry(selected)}
                 />
 
+                {/** Näytetään valittu maa alapuolella toistaiseksi*/}
                 <p>
                 Selected: {selectedCountry?.value?.common || "No country selected"}
                 </p> 
+
+                {/** Nappi josta siirrytään infosivulle ja välitetään valittu maa samalla*/}
+                <Button variant="outline-dark" onClick={() => navigate("/info", {state: {country: selectedCountry?.value}})}>Get info</Button>
 
         </div>
     );
 };
 
 export default Menu
-
-//  {selectedCountry && <Info country={selectedCountry.value} />}
