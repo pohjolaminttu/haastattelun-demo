bce26c09f25f39d3d6b02dcee315ce6b88417c30
Minttu Pohjola <minttu.pohjola@gmail.com>
2026-03-02 14:07:52 +0200

    Maiden nimet haetaan palvelimelta ja tulostetaan / sivulle, jonne lisätään seuraavaksi  alasvetovalikko

--- src/pages/info.jsx
+++ src/pages/info.jsx
@@ -1,7 +1,22 @@
 //Sivu, jolla näytetään tilamuuttujassa olevan maan infot (jotka haetaan palvelimelta)
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const Info = () => {
-    return (null);
+const Info = (country) => {
+    const [infos, setInfos] = useState([]);
+    useEffect(() => {
+        console.log('Effect for axios')
+        axios
+            .get(`https://restcountries.com/v3.1/all?fields=name/${country}`)
+            .then(response => {
+                console.log("Promise fulfilled :)")
+                setInfos(response.data)
+            })
+    }, []);
+
+    return (
+        <p>{infos}</p>
+    );
 };
 
 export default Info
--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -1,17 +1,34 @@
 //Lähtötilanne, jossa valitaan maa (sekä ylläpidetään tilamuuttujana valittua maata) ja siirrytään sivulle /info
 
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
+import axios from "axios";
 import Info from "./info";
 
 const Menu = () => {
-    const [country, setCountry] = useState("");
+    const [selectedCountry, setSelectedCountry] = useState([]);
+    const [countryNames, setCountryNames] = useState([]);
+
+    useEffect(() => {
+        console.log('Effect for axios')
+        axios
+            .get("https://restcountries.com/v3.1/all?fields=name")
+            .then(response => {
+                console.log("Promise fulfilled :)")
+                setCountryNames(response.data)
+            })
+    }, []);
 
     return (
-
-        
-        < Info />
+        <div>
+            {countryNames.map((country, index) => (
+                <div key={index}>
+                    {country.name.common}
+                    </div>
+            ))};
+        </div>
     );
-
 };
 
 export default Menu
+
+//< Info country={selectedCountry}/>
